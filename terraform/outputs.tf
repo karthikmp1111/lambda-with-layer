cdb887ed973b41fc4aacb42b345466fff9f99ea5
karthikeyan <karthikeyan@bicsglobal.com>
2025-04-10 15:35:00 +0530

    test4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,23 @@
+# output "lambda_arns" {
+#   value = { for k, v in aws_lambda_function.lambda : k => v.arn }
+# }
+
+# output "lambda_versions" {
+#   value = { for k, v in aws_lambda_function.lambda : k => v.version }
+# }
+
 output "lambda_arns" {
-  value = { for k, v in aws_lambda_function.lambda : k => v.arn }
+  value = {
+    lambda1 = aws_lambda_function.lambda1.arn
+    lambda2 = aws_lambda_function.lambda2.arn
+    lambda3 = aws_lambda_function.lambda3.arn
+  }
 }
 
 output "lambda_versions" {
-  value = { for k, v in aws_lambda_function.lambda : k => v.version }
+  value = {
+    lambda1 = aws_lambda_function.lambda1.version
+    lambda2 = aws_lambda_function.lambda2.version
+    lambda3 = aws_lambda_function.lambda3.version
+  }
 }
