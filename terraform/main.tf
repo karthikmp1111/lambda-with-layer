9256b24d8e75f9f0645fd9dbf5744830d6753eee
karthikeyan <karthikeyan@bicsglobal.com>
2025-04-10 15:32:37 +0530

    test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,26 +17,26 @@ resource "aws_iam_role" "lambda_role" {
 
 # Create Lambda Layer 1 from S3
 resource "aws_lambda_layer_version" "lambda_layer1" {
-  layer_name  = "layer1"
-  s3_bucket   = "bg-kar-terraform-state"
-  s3_key      = "lambda-layers/layer1/package.zip"
-  compatible_runtimes = ["python3.8"]
+  layer_name           = "layer1"
+  s3_bucket            = "bg-kar-terraform-state"
+  s3_key               = "lambda-layers/layer1/package.zip"
+  compatible_runtimes  = ["python3.8"]
 }
 
 # Create Lambda Layer 2 from S3
 resource "aws_lambda_layer_version" "lambda_layer2" {
-  layer_name  = "layer2"
-  s3_bucket   = "bg-kar-terraform-state"
-  s3_key      = "lambda-layers/layer2/package.zip"
-  compatible_runtimes = ["python3.8"]
+  layer_name           = "layer2"
+  s3_bucket            = "bg-kar-terraform-state"
+  s3_key               = "lambda-layers/layer2/package.zip"
+  compatible_runtimes  = ["python3.8"]
 }
 
 # Create Lambda Layer 3 from S3
 resource "aws_lambda_layer_version" "lambda_layer3" {
-  layer_name  = "layer3"
-  s3_bucket   = "bg-kar-terraform-state"
-  s3_key      = "lambda-layers/layer3/package.zip"
-  compatible_runtimes = ["python3.8"]
+  layer_name           = "layer3"
+  s3_bucket            = "bg-kar-terraform-state"
+  s3_key               = "lambda-layers/layer3/package.zip"
+  compatible_runtimes  = ["python3.8"]
 }
 
 # Create Lambda Function 1
@@ -104,25 +104,6 @@ resource "aws_lambda_function" "lambda3" {
     aws_lambda_layer_version.lambda_layer3.arn
   ]
 }
-
-# Output the ARNs of Lambda Functions
-output "lambda_arns" {
-  value = {
-    lambda1 = aws_lambda_function.lambda1.arn
-    lambda2 = aws_lambda_function.lambda2.arn
-    lambda3 = aws_lambda_function.lambda3.arn
-  }
-}
-
-# Output the versions of Lambda Functions
-output "lambda_versions" {
-  value = {
-    lambda1 = aws_lambda_function.lambda1.version
-    lambda2 = aws_lambda_function.lambda2.version
-    lambda3 = aws_lambda_function.lambda3.version
-  }
-}
-
 
 ########Test File###########
 # resource "aws_s3_bucket_object" "test_file" {
